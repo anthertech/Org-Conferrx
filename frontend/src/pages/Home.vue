782fe75b777470931afc747cbfe88a4e9a16a39e
Nihalroshan55 <nihalroshan55@gmail.com>
2024-10-11 21:40:19 +0530

    fixed conflicts

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,25 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <h1>Check this</h1>
-</template>
-
-<script setup>
-import { ref } from 'vue'
-import { Dialog, createListResource } from 'frappe-ui'  // Consolidate imports from frappe-ui
-import { session } from '../data/session'
-
-// Declare ConferList using const
-const ConferList = createListResource({
-  doctype: "Confer",
-  fields: ["name", "start_date"],
-  auto: true,
-  onSuccess(data) {
-    console.log(data);
-  }
-})
-
-</script>
-=======
     <div class="w-full h-full">
         <!-- <Navbar/>
         <Banner/> -->
@@ -58,4 +37,3 @@ const ConferList = createListResource({
     }
   });
   </script>
->>>>>>> dd6ca4d (update frontend files)
