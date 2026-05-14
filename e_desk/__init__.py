472292d8e753e7d9f736e013f5da65a4f751ca88
Hopesonjames <hopesonjames2018@gmail.com>
2026-05-14 12:22:20 +0530

    fix:init

--- e_desk/__init__.py
+++ e_desk/__init__.py
@@ -1,14 +1,27 @@
 
-import frappe
-from frappe.utils.user import is_website_user
+# import frappe
+# from frappe.utils.user import is_website_user
+
+# __version__ = '0.0.1'
+
+# def check_app_permission():
+# 	if frappe.session.user == "Administrator":
+# 		return True
+
+# 	if is_website_user():
+# 		return False
+
+# 	return True
+
+
 
 __version__ = '0.0.1'
 
 def check_app_permission():
-	if frappe.session.user == "Administrator":
-		return True
-
-	if is_website_user():
-		return False
-
-	return True
+    import frappe
+    from frappe.utils.user import is_website_user
+    if frappe.session.user == "Administrator":
+        return True
+    if is_website_user():
+        return False
+    return True
