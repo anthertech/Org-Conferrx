cd08df3afe2504351feab1781443c4bcab330712
akhilakr113 <akhilaraveeendran@gmail.com>
2026-07-20 06:50:31 +0000

    feat: event particpant history report

---
+++ e_desk/e_desk/report/event_history/event_history.js
@@ -0,0 +1,29 @@
+// Copyright (c) 2026, Anther Technologies Pvt Ltd and contributors
+// For license information, please see license.txt
+
+
+frappe.query_reports["Event History"] = {
+    filters: [
+        {
+            fieldname: "to_date",
+            label: __("To Date"),
+            fieldtype: "Date",
+            reqd: 1,
+            default: frappe.datetime.get_today(),
+        },
+        {
+            fieldname: "email",
+            label: __("User"),
+            fieldtype: "Link",
+            options: "User",
+            default: frappe.session.user
+        },
+        {
+            fieldname: "event",
+            label: __("Event"),
+            fieldtype: "Link",
+            options: "Conference",
+        }
+    ],
+};
+
