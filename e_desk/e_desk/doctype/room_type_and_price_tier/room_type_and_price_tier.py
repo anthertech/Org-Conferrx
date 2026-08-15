1da25cb143e710323acea4e83060d56db1fc5c50
Anjana <anjana@anther.tech>
2026-08-15 19:17:12 +0530

    feat: add Room Type and Price Tier doctype

---
+++ e_desk/e_desk/doctype/room_type_and_price_tier/room_type_and_price_tier.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Anther Technologies Pvt Ltd and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class RoomTypeandPriceTier(Document):
+	_DOCTYPE_NAME = "Room Type and Price Tier"
